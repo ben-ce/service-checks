fe3d13fc49fb361d1ff699b9259c87b1caae9cde
ben-ce <bence.schmehl@protonmail.com>
2018-11-09 12:45:16 +0100

    indent & typo fix

--- logmonitor.sh
+++ logmonitor.sh
@@ -1,39 +1,44 @@
 #!/bin/bash
 
-today=`date +"%Y-%m-%d"`
-logparse="$HOME/logparse"
-logpath='/var/log/nginx'
+today=$(date +"%Y-%m-%d")
+workdir="$HOME/logmonitor"
+logparse="$workdir/logparse"
+logpath="/var/log/nginx"
+scriptlog="$workdir/script.log"
+mailtext="$workdir/mail.txt"
 
-mail_command(){
-	/usr/sbin/sendmail -t < mail.txt
-}
-
+if [[ ! -d "$logparse" ]]; then
+    mkdir -p $logparse
+fi
+    
 # match access log file contents, igy mindig megvan a teljes lista, de csak egyszer
 for i in $(ls -d -1 -t $logpath/access.log*);do
-	zgrep -B 1 "GET /enyem[.]html" $i >> $logparse/logmatch-$today
+    zgrep -B 1 "GET /enyem[.]html" $i >> $logparse/logmatch-$today
 done
 
 # determine last two days' logmatch files
 file1=$(ls -1t $logparse/ | head -n 1)
 file2=$(ls -1t $logparse/ | head -n 2 | tail -n 1)
 
-filediff=$(diff $logparse/$file1 $logdir/$file2)
+filediff=$(diff $logparse/$file1 $logparse/$file2)
 
 # alert if any new matching lines are present
 if [[ -z $filediff ]]; then
-	echo "file diff empty, no new matching lines"
+    echo "$today : file diff empty, no new matching lines" >> $scriptlog
 else 
-	echo "if statement not true"
+    echo "$today : new matching line, composing mail.txt ..." >> $scriptlog
+
+    # compose mail
+    echo "To:pentesters@govcert.hu" > $mailtext
+    echo "From:account@domain.net" >> $mailtext
+    echo "Subject: logmonitor alert" >> $mailtext
+    echo "" >> $mailtext
+    echo "$filediff" >> $mailtext
 	
-	# compose mail
-	echo "To:pentesters@govcert.hu" > mail.txt
-	echo "From:account@domain.net" >> mail.txt
-	echo "Subject: logmonitor alert" >> mail.txt
-	echo "" >> mail.txt
-	echo "$filediff" >> mail.txt
-	# mail_command() # TODO define mail_command function
+    # sendmail -t < $mailtext && echo "$today : mail sent" >> $scriptlog || echo "$today : mail command error" >> $scriptlog
+    # TODO configure /etc/ssmtp/ssmtp.conf with mailserver data + account
 fi
 
 # keep only the last logmatch file
 rm $logparse/$file2
-mv $logparse/$file1 $logdir/$file1.old
+mv $logparse/$file1 $logparse/$file1.old
